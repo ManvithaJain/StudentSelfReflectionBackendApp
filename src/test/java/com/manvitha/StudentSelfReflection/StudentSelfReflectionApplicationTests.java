5b0113c3485ea6b58949202668cce4900900e63d
ManvithaJain <manvithajain788@gmail.com>
2025-04-17 16:40:53 +0530

    Getting Started with StudentSelfReflection Backend Project

---
+++ src/test/java/com/manvitha/StudentSelfReflection/StudentSelfReflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manvitha.StudentSelfReflection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSelfReflectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
