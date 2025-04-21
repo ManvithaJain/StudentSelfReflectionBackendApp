a5d2ce1860eb4186ffd9408b5fa9e5c3c57cda72
ManvithaJain <manvithajain788@gmail.com>
2025-04-21 16:25:46 +0530

    Created ApplicationUser and Course java file

---
+++ src/main/java/com/manvitha/StudentSelfReflection/model/ApplicationUser.java
@@ -0,0 +1,22 @@
+package com.manvitha.StudentSelfReflection.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class ApplicationUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long applicationUserId;
+    private String username;
+    private String password;
+    private String email;
+}
